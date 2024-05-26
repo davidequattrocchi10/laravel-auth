53894b773f2f566a35bc8c3a3cb8fd77c3d4715c
davidequattrocchi10 <davidequattrocchi10@gmail.com>
2024-05-26 19:16:34 +0200

    Add relationships between Project and Type models

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,10 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Project extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title', 'slug', 'description', 'final_goal', 'area', 'cover_image'];
+    protected $fillable = ['title', 'type_id', 'slug', 'description', 'final_goal', 'area', 'cover_image'];
+
+
+    public function type(): BelongsTo
+    {
+        return $this->belongsTo(Type::class);
+    }
 }
