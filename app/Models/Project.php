cc1e49605836638f0ea3b64f1542b27879b3953d
davidequattrocchi10 <davidequattrocchi10@gmail.com>
2024-05-27 14:43:14 +0200

    Add one to many relation between users and projects

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -17,4 +17,9 @@ class Project extends Model
     {
         return $this->belongsTo(Type::class);
     }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
